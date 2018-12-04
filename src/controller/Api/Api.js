f3de95fc871390ddd342c44b77695af8e888d3d0
Horst Jahns <horstjahns@gmail.com>
2018-12-04 09:40:25 +0100

    removed api and unused utility function

--- src/controller/Api/Api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default class api {
-  static fetchRestaurants(port = '') {
-    const url = '/api/restaurants' + port;
-    const request = axios.get(url);
-    return request;
-  }
-}
--- src/controller/Utility/Utility.js
+++ src/controller/Utility/Utility.js
@@ -1,17 +1,4 @@
 export default class Utility {
-  static structureGroupsByProperty(array, property) {
-    let structuredObj = [];
-    array.map((obj, index) => {
-      let value = obj[property];
-      if(!(value in structuredObj)) {
-        structuredObj[value] = [];
-      }
-      structuredObj[value].push(obj);
-      return obj;
-    });
-    return structuredObj;
-  }
-
   static createPropertyGroups(propObj, array, property) {
     for (let p in propObj) {
       for (let i = 0; i < array.length; i++) {
